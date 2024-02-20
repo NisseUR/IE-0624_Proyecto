e130c11010d2fe20dd5d870551f51bde46ccde3c
Alonso J Jiménez A <alonso.jimenezanchia@ucr.ac.cr>
2024-02-20 14:53:27 -0600

    Desarrollo sketch the Arduino para hablar con ThingsBoard

--- Pruebas/chime_mode/chime_mode.ino
+++ Pruebas/chime_mode/chime_mode.ino
@@ -5,7 +5,7 @@ const byte COLS = 4; // Cuatro columnas
 char keys[ROWS][COLS] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
-  {'7','8','9','C'},
+  {'7','8','9','C'},.
   {'*','0','#','D'}
 };
 byte rowPins[ROWS] = {9, 8, 7, 6}; // Pines de las filas del teclado a Arduino
